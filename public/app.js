a8f9a5e04e25d8948b010fbb01812a533cd6f606
Juniper Chapman <junipie@gmail.com>
2015-10-30 10:09:32 -0600

    TruckList pulls from the database, preparing to add same functionality to truck profile page

--- public/app.js
+++ public/app.js
@@ -1,133 +1,15 @@
-var newTruck = {trucks: [ 
-    {truckName: "Noodle Wagon",
-        city: "Missoula, MT",
-        description: "A Friendly Asian Food Truck",
-        cuisine: ["Asian"],
-        currentLocation: undefined,
-        monTime: "9-5",
-        tuesTime: "9-5",
-        wedTime: "9-5",
-        thurTime: "9-5",
-        friTime: "9-5",
-        satTime: "9-5",
-        sunTime: "9-5",
-        timeCategory: undefined,
-        payment: undefined,
-        foodOptions: ["Vegan", "Organic", "Gluten Free"],
-        facebook: undefined,
-        twitter: undefined},
-    {truckName: "El Cazador",
-        city: "Missoula, MT",
-        description: "A Friendly Mexican Food Truck",
-        cuisine: ["Mexican"],
-        currentLocation: undefined,
-        monTime: "9-5",
-        tuesTime: "9-5",
-        wedTime: "9-5",
-        thurTime: "9-5",
-        friTime: "9-5",
-        satTime: "9-5",
-        sunTime: "9-5",
-        timeCategory: undefined,
-        payment: undefined,
-        foodOptions: ["Vegan", "Organic", "Gluten Free"],
-        facebook: undefined,
-        twitter: undefined},
-    {truckName: "Big Dipper",
-        city: "Missoula, MT",
-        description: "A Friendly Mexican Food Truck",
-        cuisine: ["Dessert"],
-        currentLocation: undefined,
-        monTime: "9-5",
-        tuesTime: "9-5",
-        wedTime: "9-5",
-        thurTime: "9-5",
-        friTime: "9-5",
-        satTime: "9-5",
-        sunTime: "9-5",
-        timeCategory: undefined,
-        payment: undefined,
-        foodOptions: ["Vegan", "Organic", "Gluten Free"],
-        facebook: undefined,
-        twitter: undefined},
-    {truckName: "Bob's Food Truck",
-        city: "Missoula, MT",
-        description: "A Friendly Mexican Food Truck",
-        cuisine: ["American"],
-        currentLocation: undefined,
-        monTime: "9-5",
-        tuesTime: "9-5",
-        wedTime: "9-5",
-        thurTime: "9-5",
-        friTime: "9-5",
-        satTime: "9-5",
-        sunTime: "9-5",
-        timeCategory: undefined,
-        payment: undefined,
-        foodOptions: ["Vegan", "Organic", "Gluten Free"],
-        facebook: undefined,
-        twitter: undefined},
-    {truckName: "Dobi Noodles",
-        city: "Missoula, MT",
-        description: "A Friendly Mexican Food Truck",
-        cuisine: ["Asian"],
-        currentLocation: undefined,
-        monTime: "9-5",
-        tuesTime: "9-5",
-        wedTime: "9-5",
-        thurTime: "9-5",
-        friTime: "9-5",
-        satTime: "9-5",
-        sunTime: "9-5",
-        timeCategory: undefined,
-        payment: undefined,
-        foodOptions: ["Vegan", "Organic", "Gluten Free"],
-        facebook: undefined,
-        twitter: undefined}
-        ]
-      }
 
-//var StudentPanels = React.createclassName({
-//    render: function() {
-//        var students = this.props.data.items.map(function(k){
-//        return (
-//				<div>
-//                   <div classNameName="col-md-4">
-//                   <div classNameName="panel panel-default">
-//                       <div classNameName="panel-heading">
-//                           <h3 classNameName="panel-title">Student {k.id}</h3>
-//                       </div>
-//                       <div classNameName="panel-body">
-//                           <p>{k.name}</p>
-//                           <p>{k.email}</p>
-//                           <p>{k.gpa}</p>
-//                       </div>
-//                   </div>
-//                   </div>
-//				</div>
-//        	)
-//    });
-//        return (
-//            <div>
-//            {students}
-//            </div>
-//            );
-//    }
-//});
-//
-
-var App = React.createClass({
+var TruckList = React.createClass({
     render: function() {
-        var trucks = this.props.data.trucks.map(function(k){
+        var oneTruck = this.props.data.map(function(truck){
             return (
                 <div>
-                    
                     <div className="well clearfix">
                       <div className="col-sm-9 hidden-xs">
-                        <h2>{k.truckName}</h2><br></br>
+                        <h2>{truck.truckName}</h2><br></br>
                         <div className="row">
                             <div className='col-xs-4'>
-                                <div className="well well-orange-cuisine text-center truckList-cuisine-med">{k.cuisine[0]}</div>
+                                <div className="well well-orange-cuisine text-center truckList-cuisine-sm">{truck.cuisine}</div>
                             </div>
                         </div>
                       </div>
@@ -153,10 +35,10 @@ var App = React.createClass({
             
                       <div className="col-xs-12 hidden-sm hidden-md hidden-lg">
                         <img src="img/openSign.png" className="hidden-sm truckList-open-xs"/>
-                        <h2>{k.truckName}</h2><br></br>
+                        <h2>{truck.truckName}</h2><br></br>
                         <div className="row">
                             <div className='col-xs-4 hidden-sm hidden-md hidden-lg'>
-                                <div className="well well-orange-cuisine text-center truckList-cuisine-sm">{k.cuisine[0]}</div>
+                                <div className="well well-orange-cuisine text-center truckList-cuisine-sm">{truck.cuisine}</div>
                             </div>
                             </div>
                         </div>
@@ -183,11 +65,47 @@ var App = React.createClass({
         });
         
         return (
-				<div>
-                    {trucks}
+                <div>
+                    {oneTruck}
                 </div>
-        	);
+            );
     }
 });
 
-React.render(<App data={newTruck}/>, document.getElementById('react-box'))
+
+var TruckBox = React.createClass({
+    //Set initial state-----------------
+    getInitialState: function(){
+        return{data: []};
+    },
+    //Fetch data from our server--------------
+    loadTrucksFromServer: function(){
+        $.ajax({
+            url:this.props.url,
+            dataType:"json",
+            cache: false,
+            success: function(data){
+                console.log("inside success")
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err){
+                console.log("broken url is " + this.props.url);
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    //Mount components------------------
+    componentDidMount: function(){
+        this.loadTrucksFromServer();
+    },
+    render: function() {
+        return (
+            <div>
+                <TruckList data={this.state.data}/>
+            </div>
+        );
+    }
+});
+
+
+React.render(<TruckBox url="/api/trucks/"/>, document.getElementById("react-box"));
